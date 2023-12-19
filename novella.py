697205265c37962434365ab74df4865b59913de5
dronoff2032 <145379850+dronoff2032@users.noreply.github.com>
2023-12-19 14:04:46 +0300

    Add files via upload

--- novella.py
+++ novella.py
@@ -1,8 +1,33 @@
 import time
 import random
+import json
+import csv
 
-demonstrationChoice = None
-mayorChoice = None
+with open('name.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    name = None
+    while (name == None):
+        name = str(input("Введите свое имя: "))
+    writer.writerow(name)
+
+defaultdata = {
+        'choice': 0,
+        'mayorChoice': 0,
+        'demonstrationChoice': 0
+}
+
+def readdata():
+    with open('save.json', 'r') as file:
+        data = json.load(file)
+    return data
+
+def writedata(data):
+    with open('save.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+choice = readdata()['choice']
+demonstrationChoice = readdata()['demonstrationChoice']
+mayorChoice = readdata()['mayorChoice']
 
 def newline(text):
     print(text)
@@ -68,7 +93,6 @@ def print_outcomes(win):
         for detail in outcome['details']:
             newline(detail)
 
-#def check_ending()
 
 def print_demonstrations_variants():
     variants = [
@@ -108,87 +132,100 @@ def print_demonstrations_outcomes(win):
     if outcome:
         newline(outcome['text'])
 
+if (choice == 0):
+    newline("Вы - обычный житель маленького города..")
+    newline("Сегодня наступил день важных выборов мэра.")
+    choice = None
+    while (choice != "Да" and choice != "Нет"):
+        choice = input("Хотите ли вы проголосовать? (Да или Нет): ")
+    existdata = readdata()
+    existdata['choice'] = choice
+    writedata(existdata)
 
-newline("Вы - обычный житель маленького города..")
-newline("Сегодня наступил день важных выборов мэра.")
+if (mayorChoice == 0):
+    if (choice == "Да"):
+        newline("Вы решаете проголосовать за одного из кандидатов.\n\n")
 
-choice = None
-while (choice != "Да" and choice != "Нет"):
-    choice = input("Хотите ли вы проголосовать? (Да или Нет): ")
+        print_candidates()
 
-if (choice == "Да"):
-    newline("Вы решаете проголосовать за одного из кандидатов.\n\n")
+        while (mayorChoice != 1 and mayorChoice != 2 and mayorChoice != 3):
+            try:
+                mayorChoice = int(input("Выберите кандидата, за которого хотите проголосовать (1-3): "))
+            except ValueError:
+                print("Пожалуйста, введите число от 1 до 3.")
+    else:
+        mayorChoice = None
+        print("Вы приняли решение воздержаться от своего голоса.")
 
-    print_candidates()
+    mayorChoiceRand = random.randint(1, 3)
+    newline("\nВ скором времени вам стали известны результаты голосования.")
 
-    while (mayorChoice != 1 and mayorChoice != 2 and mayorChoice != 3):
+    if (mayorChoice != None and mayorChoice != mayorChoiceRand):
+        newline("К сожалению, ваше голос ничего не решил.")
+
+    print_outcomes(mayorChoiceRand)
+
+    if (mayorChoice != None and mayorChoice == mayorChoiceRand):
+        newline("\n\nВы сделали правильный выбор.")
+
+    print("<= Следующий день =>")
+
+    newline("\nНа следующий день город просыпается под властью нового мэра.")
+    newline("Все жители в ожидании перемен.")
+    newline("Первые дни проходят под знаком активных реформ и изменений в городской жизни.")
+
+    if mayorChoice == 1:
+        print("\nСтарый политик принимает жесткие меры, начиная с борьбы с преступностью и коррупцией.")
+        print("Город становится более дисциплинированным, но не все жители одобряют такой подход.")
+    elif mayorChoice == 2:
+        print("\nМолодой мэр приносит в город свежий взгляд и начинает внедрять инновации.")
+        print("Он становится сторонником открытости и прозрачности.")
+        print("Граждане вдохновляются его красочными выступлениями и активно участвуют в общественной жизни.")
+    else:
+        print("\nЗагадочный мэр создает атмосферу экзотики и нестандартных решений.")
+        print("Привычные порядки нарушаются, и город становится местом для развития уникальных идей.")
+        print("Хотя не все поддерживают его, те, кто разделяет его видение, считают это интересным временем для города.")
+
+    newline("\nС каждым днем мэр продолжает внедрять свои идеи, но сопротивление со стороны оппозиции усиливается.")
+    newline("Горожане оказываются втянутыми в волнующие события, и жизнь в городе продолжает меняться.")
+    newline("В один день вы узнаете о двух согласованых митингах: в поддержку нового мера и против.\n")
+    existdata = readdata()
+    existdata['mayorChoice'] = mayorChoice
+    writedata(existdata)
+
+if (demonstrationChoice == 0):
+    print_demonstrations_variants()
+    while (demonstrationChoice != 1 and demonstrationChoice != 2 and demonstrationChoice != 3):
         try:
-            mayorChoice = int(input("Выберите кандидата, за которого хотите проголосовать (1-3): "))
+            demonstrationChoice = int(input("Решите как поступите в данной ситуации (1-3): "))
         except ValueError:
-            print("Пожалуйста, введите число от 1 до 3.")
-else:
-    mayorChoice = None
-    print("Вы приняли решение воздержаться от своего голоса.")
+                print("Пожалуйста, введите число от 1 до 3.")
 
-mayorChoiceRand = random.randint(1, 3)
-newline("\nВ скором времени вам стали известны результаты голосования.")
+    print_demonstrations_outcomes(demonstrationChoice)
 
-if (mayorChoice != None and mayorChoice != mayorChoiceRand):
-    newline("К сожалению, ваше голос ничего не решил.")
+    if (mayorChoice != None and mayorChoice == mayorChoiceRand and demonstrationChoice != None and demonstrationChoice == 1):
+        newline("В итоге оппозиция смирилась с курсом действующего мера и признала поражение.")
+        newline("В том числе благодаря вашему участию вы сделали город лучше.")
+    elif (mayorChoice != None and mayorChoice == mayorChoiceRand and demonstrationChoice != None and demonstrationChoice == 2):
+        newline("В конце концов митинг ничего не решил и участники мирно разошлись.")
+        newline("Несмотря на ваше участие в городе продолжилась активная политическая жизнь.")
+    elif (mayorChoice != None and mayorChoice != mayorChoiceRand and demonstrationChoice != None and demonstrationChoice == 1):
+        newline("Хотя это было неожиданным решением, но митинг ничего не решил и участники мирно разошлись.")
+        newline("Несмотря на ваше участие в городе продолжилась активная политическая жизнь.")
+    elif (mayorChoice != None and mayorChoice != mayorChoiceRand and demonstrationChoice != None and demonstrationChoice == 2):
+        newline("В итоге оппозиция вынудила действующего мера и его сторонников пойти к вам на встречу.")
+        newline("В том числе благодаря вашему участию вы сделали город лучше.")
+    else:
+        newline("Хотя вы и решили пронаблюдать со стороны, но в итоге вам стало скучно.")
+        newline("В конце концов вы перестали интересоваться политикой и сосредоточились на личной жизни.")
+    existdata = readdata()
+    existdata['demonstrationChoice'] = demonstrationChoice
+    writedata(existdata)
 
-print_outcomes(mayorChoiceRand)
-
-if (mayorChoice != None and mayorChoice == mayorChoiceRand):
-    newline("\n\nВы сделали правильный выбор.")
-
-print("<= Следующий день =>")
-
-newline("\nНа следующий день город просыпается под властью нового мэра.")
-newline("Все жители в ожидании перемен.")
-newline("Первые дни проходят под знаком активных реформ и изменений в городской жизни.")
-
-if mayorChoice == 1:
-    print("\nСтарый политик принимает жесткие меры, начиная с борьбы с преступностью и коррупцией.")
-    print("Город становится более дисциплинированным, но не все жители одобряют такой подход.")
-elif mayorChoice == 2:
-    print("\nМолодой мэр приносит в город свежий взгляд и начинает внедрять инновации.")
-    print("Он становится сторонником открытости и прозрачности.")
-    print("Граждане вдохновляются его красочными выступлениями и активно участвуют в общественной жизни.")
-else:
-    print("\nЗагадочный мэр создает атмосферу экзотики и нестандартных решений.")
-    print("Привычные порядки нарушаются, и город становится местом для развития уникальных идей.")
-    print("Хотя не все поддерживают его, те, кто разделяет его видение, считают это интересным временем для города.")
-
-newline("\nС каждым днем мэр продолжает внедрять свои идеи, но сопротивление со стороны оппозиции усиливается.")
-newline("Горожане оказываются втянутыми в волнующие события, и жизнь в городе продолжает меняться.")
-newline("В один день вы узнаете о двух согласованых митингах: в поддержку нового мера и против.\n")
-
-print_demonstrations_variants()
-
-
-while (demonstrationChoice != 1 and demonstrationChoice != 2 and demonstrationChoice != 3):
-    try:
-        demonstrationChoice = int(input("Решите как поступите в данной ситуации (1-3): "))
-    except ValueError:
-            print("Пожалуйста, введите число от 1 до 3.")
-
-print_demonstrations_outcomes(demonstrationChoice)
-
-if (mayorChoice != None and mayorChoice == mayorChoiceRand and demonstrationChoice != None and demonstrationChoice == 1):
-    newline("В итоге оппозиция смирилась с курсом действующего мера и признала поражение.")
-    newline("В том числе благодаря вашему участию вы сделали город лучше.")
-elif (mayorChoice != None and mayorChoice == mayorChoiceRand and demonstrationChoice != None and demonstrationChoice == 2):
-    newline("В конце концов митинг ничего не решил и участники мирно разошлись.")
-    newline("Несмотря на ваше участие в городе продолжилась активная политическая жизнь.")
-elif (mayorChoice != None and mayorChoice != mayorChoiceRand and demonstrationChoice != None and demonstrationChoice == 1):
-    newline("Хотя это было неожиданным решением, но митинг ничего не решил и участники мирно разошлись.")
-    newline("Несмотря на ваше участие в городе продолжилась активная политическая жизнь.")
-elif (mayorChoice != None and mayorChoice != mayorChoiceRand and demonstrationChoice != None and demonstrationChoice == 2):
-    newline("В итоге оппозиция вынудила действующего мера и его сторонников пойти к вам на встречу.")
-    newline("В том числе благодаря вашему участию вы сделали город лучше.")
-else:
-    newline("Хотя вы и решили пронаблюдать со стороны, но в итоге вам стало скучно.")
-    newline("В конце концов вы перестали интересоваться политикой и сосредоточились на личной жизни.")
-
-
-print("\n\nКонец игры.")
+name = []
+with open('name.csv', 'r', newline='') as csvfile:
+    get = csv.reader(csvfile)
+    for row in get:
+        name.append(row)
+    result = (''.join(name[0]))
+    print(f'\n\nИгрок {result}, поздравляем с прохождением игры!')
